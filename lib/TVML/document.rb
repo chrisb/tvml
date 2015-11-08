fc17535d281c9a8fa4f8f871c77f3b33b36c184f
Chris Bielinski <cbielinski@payoff.com>
2015-11-08 13:27:50 -0800

    add ability to build with JS escaping

--- lib/TVML/document.rb
+++ lib/TVML/document.rb
@@ -2,6 +2,9 @@ module TVML
   class Document
     attr_accessor :builder, :template
 
+    JS_ESCAPE_MAP	=	{ '\\' => '\\\\', '</' => '<\/', "\r\n" => '\n',
+                      "\n" => '\n', "\r" => '\n', '"' => '\\"', "'" => "\\'" }
+
     def initialize
       @builder = Builder::XmlMarkup.new
       @builder.instruct!
@@ -11,6 +14,17 @@ module TVML
       @builder.document do |doc|
         doc << @template.build if @template
       end
+    end
+
+    def build_escaped
+      escape_javascript(build)
+    end
+
+    private
+
+    def escape_javascript(javascript)
+      return '' unless javascript
+      javascript.gsub(%r{(\|<\/|\r\n|\342\200\250|\342\200\251|[\n\r"'])}) { |match| JS_ESCAPE_MAP[match] }
     end
   end
 end
--- spec/TVML/document_spec.rb
+++ spec/TVML/document_spec.rb
@@ -8,4 +8,11 @@ describe TVML::Document do
     end
     expect(subject.build).to eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?><document><alertTemplate><description>How are things?</description><title>Hello World</title></alertTemplate></document>")
   end
+
+  it 'can escape a document' do
+    subject.template = TVML::Template::Alert.new.tap do |alert|
+      alert.title = "Hello World"
+    end
+    expect(subject.build_escaped).to eq("<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?><document><alertTemplate><title>Hello World</title></alertTemplate></document>")
+  end
 end
