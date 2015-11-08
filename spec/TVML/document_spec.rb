6eb59a60d7f5cfb942d299e7402a1877e103c346
Chris Bielinski <cbielinski@payoff.com>
2015-11-08 12:45:32 -0800

    change spec to match README example

--- spec/TVML/document_spec.rb
+++ spec/TVML/document_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe TVML::Document do
   it 'can build a document' do
-    subject.template = TVML::Template::Alert.new
-    subject.template.title = "Hello World"
-    subject.template.description = "How are things?"
+    subject.template = TVML::Template::Alert.new.tap do |alert|
+      alert.title = "Hello World"
+      alert.description = "How are things?"
+    end
     expect(subject.build).to eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?><document><alertTemplate><description>How are things?</description><title>Hello World</title></alertTemplate></document>")
   end
 end
